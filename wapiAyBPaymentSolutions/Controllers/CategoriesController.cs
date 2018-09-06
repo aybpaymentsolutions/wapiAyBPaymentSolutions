28ae939122dd293583efa27953be1b0af1f0495d
Bayron Rodezno <bayron.rodezno@gmail.com>
2018-09-06 13:08:48 -0500

    Category Menu List
    In this version we have added the methods, classes and models to be able to show the categories's list

---
+++ wapiAyBPaymentSolutions/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using wapiAyBPaymentSolutions.Models.JsonClasses;
+using wapiAyBPaymentSolutions.Models;
+
+namespace wapiAyBPaymentSolutions.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        [Route("api/Categories/{storeID}")]
+        [HttpGet]
+        public CategoriesResponse getListCategories(string storeID)
+        {
+            var response = new MenuCatModel();
+            return response.getCategories(storeID);
+        }
+    }
+}
---
+++ wapiAyBPaymentSolutions/Models/JsonClasses/CategoriesResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace wapiAyBPaymentSolutions.Models.JsonClasses
+{
+
+    public partial class CategoriesResponse
+    {
+        [JsonProperty("responseCode")]
+        public string ResponseCode { get; set; }
+
+        [JsonProperty("responseMessage")]
+        public string ResponseMessage { get; set; }
+
+        [JsonProperty("infoCategories")]
+        public List<InfoCategories> InfoCategories { get; set; }
+    }
+
+    public partial class InfoCategories
+    {
+        [JsonProperty("MenuCategoryID")]
+        public int MenuCategoryID { get; set; }
+        [JsonProperty("MenuCategoryText")]
+        public string MenuCategoryText { get; set; }
+        [JsonProperty("MenuCategoryInActive")]
+        public bool MenuCategoryInActive { get; set;}
+        [JsonProperty("StoreID")]
+        public string StoreID { get; set; }
+    }
+
+}
